7117e5a0caea4b59ffc66daee5b428acfc951912
Small4 Services <small4services@gmail.com>
2025-07-28 20:57:43 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 # app.py
 from flask import Flask, request, jsonify
-import openai
-import os
 
 app = Flask(__name__)
-openai.api_key = os.environ["OPENAI_API_KEY"]
 
 @app.route("/ask", methods=["GET"])
 def get():

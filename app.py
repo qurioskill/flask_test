e5a4ba974e95f5ca3ee3dfa001568414fdfd8379
Small4 Services <small4services@gmail.com>
2025-07-30 19:37:03 -0400

    Adding Cors

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # app.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/ask", methods=["GET"])
 def get():
